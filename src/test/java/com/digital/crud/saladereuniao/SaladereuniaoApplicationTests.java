849ee61519fc12c192ad3ec8e001f390daeaea31
Ricardo Murca <richard_sm1@outlook.com>
2021-08-24 16:15:58 -0300

    Projeto criado

---
+++ src/test/java/com/digital/crud/saladereuniao/SaladereuniaoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.digital.crud.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
